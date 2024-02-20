9547268609e432286b8d93255d7fde87233169c4
TheViLkiS <thevilkis@gmail.com>
2024-02-20 14:49:02 +0400

    Initial Commit

---
+++ TZRB/TZRBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TZRBApp.swift
+//  TZRB
+//
+//  Created by Дмитрий Гусев on 20.02.24.
+//
+
+import SwiftUI
+
+@main
+struct TZRBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
